3b4c4a2b5d9ecd16ce730d3077d77d0e5fec7ed8
Luís Oliveira <luis_carlos_92@hotmail.com>
2016-01-30 15:05:41 +0000

    generate Animals

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,14 +8,18 @@ namespace Assets.Scripts.GameManager
     public class GameManager : MonoBehaviour
     {
         //public fields, seen by Unity in Editor
-        public Player PlayerOne;
-        public Player PlayerTwo;
+        public GameObject PlayerOne;
+        public GameObject PlayerTwo;
         
-        public GameObject AltarOne;
-        public GameObject AltarTwo;
+        public GameObject SpawnOne;
+        public GameObject SpawnTwo;
 
         public GameObject horseModel;
         public GameObject kittenModel;
+        public GameObject goatModel;
+        public GameObject roosterModel;
+        public GameObject sheepModel;
+        public GameObject unicornModel;
 
         //private fields
         private List<GameObject> Sacrifices;
@@ -23,23 +27,23 @@ namespace Assets.Scripts.GameManager
 
         public void Start()
         {
-            /*
+            
             for (int i = 0; i < MaxNumberOfObjectsPerPlayer; i++)
             {
                 float angle = i * Mathf.PI * 2 / MaxNumberOfObjectsPerPlayer;
-                Vector3 pos = AltarOne.transform.localPosition;
-                pos.x += (1f * Mathf.Cos(angle));
-                pos.z += (1f * Mathf.Sin(angle));
+                Vector3 pos = SpawnOne.transform.position;
+                 pos.x +=  Properties.ANIMAL_SPAWN_RADIUS * Mathf.Cos(angle);
+                 pos.z -= Properties.ANIMAL_SPAWN_RADIUS * Mathf.Sin(angle);
                 GenerateAnimal(pos);
             }
-            */
-
-           /* for (int i = 0; i < MaxNumberOfObjectsPerPlayer; i++)
+            for (int i = 0; i < MaxNumberOfObjectsPerPlayer; i++)
             {
                 float angle = i * Mathf.PI * 2 / MaxNumberOfObjectsPerPlayer;
-                Vector3 pos = new Vector3(Mathf.Cos(angle), AltarTwo.transform.position.y, Mathf.Sin(angle)) * 5f;
+                Vector3 pos = SpawnTwo.transform.position;
+                pos.x += Properties.ANIMAL_SPAWN_RADIUS * Mathf.Cos(angle);
+                pos.z += Properties.ANIMAL_SPAWN_RADIUS * Mathf.Sin(angle);
                 GenerateAnimal(pos);
-            }*/
+            }
 
             //change to different animals later ; maybe split into two different lists?
             // this.Sacrifices = GameObject.FindGameObjectsWithTag("Sacrifice").ToList();
@@ -76,13 +80,30 @@ namespace Assets.Scripts.GameManager
         {
             if(type == Properties.HORSE)
             {
-                return Instantiate(kittenModel, pos, Quaternion.identity);
+                return Instantiate(horseModel, pos, Quaternion.identity);
             }
             else if(type == Properties.CAT)
             {
-                return Instantiate(horseModel, pos, Quaternion.identity);
+                return Instantiate(kittenModel, pos, Quaternion.identity);
             }
-            return null;
+            else if (type == Properties.GOAT)
+            {
+                return Instantiate(goatModel, pos, Quaternion.identity);
+            }
+            else if (type == Properties.UNICORN)
+            {
+                return Instantiate(unicornModel, pos, Quaternion.identity);
+            }
+            else if (type == Properties.CHICKEN)
+            {
+                return Instantiate(roosterModel, pos, Quaternion.identity);
+            }
+            else if (type == Properties.SHEEP)
+            {
+                return Instantiate(sheepModel, pos, Quaternion.identity);
+            }
+
+            return Instantiate(kittenModel, pos, Quaternion.identity);
         }
 
         public void GrabAnimal(Player player, Animal animal)
