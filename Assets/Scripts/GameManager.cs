4bece198fced4fed651ba131150ab774b8a73ec1
Luís Oliveira <luis_carlos_92@hotmail.com>
2016-01-30 14:16:46 +0000

    Prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,15 +10,40 @@ namespace Assets.Scripts.GameManager
         //public fields, seen by Unity in Editor
         public Player PlayerOne;
         public Player PlayerTwo;
+        
+        public GameObject AltarOne;
+        public GameObject AltarTwo;
+
+        public GameObject horseModel;
+        public GameObject kittenModel;
 
         //private fields
         private List<GameObject> Sacrifices;
-        
+        private int MaxNumberOfObjectsPerPlayer = 6;
+
         public void Start()
-        {            
+        {
+            /*
+            for (int i = 0; i < MaxNumberOfObjectsPerPlayer; i++)
+            {
+                float angle = i * Mathf.PI * 2 / MaxNumberOfObjectsPerPlayer;
+                Vector3 pos = AltarOne.transform.localPosition;
+                pos.x += (1f * Mathf.Cos(angle));
+                pos.z += (1f * Mathf.Sin(angle));
+                GenerateAnimal(pos);
+            }
+            */
+
+           /* for (int i = 0; i < MaxNumberOfObjectsPerPlayer; i++)
+            {
+                float angle = i * Mathf.PI * 2 / MaxNumberOfObjectsPerPlayer;
+                Vector3 pos = new Vector3(Mathf.Cos(angle), AltarTwo.transform.position.y, Mathf.Sin(angle)) * 5f;
+                GenerateAnimal(pos);
+            }*/
+
             //change to different animals later ; maybe split into two different lists?
-            this.Sacrifices = GameObject.FindGameObjectsWithTag("Sacrifice").ToList();
-            
+            // this.Sacrifices = GameObject.FindGameObjectsWithTag("Sacrifice").ToList();
+
         }
 
         public void Update()
@@ -36,5 +61,43 @@ namespace Assets.Scripts.GameManager
 
         //refer to IAJ-Lab9 GameManager for additional functions
 
+        private bool CheckRange(GameObject a, GameObject b, float maximumSqrDistance)
+        {
+            return (a.transform.position - b.transform.position).sqrMagnitude <= maximumSqrDistance;
+        }
+
+        private Object GenerateAnimal(Vector3 pos)
+        {
+            int type = Random.Range(0, Properties.ANIMAL_TYPES);
+            return GenerateAnimal(type, pos);
+        }
+
+        private Object GenerateAnimal(int type, Vector3 pos)
+        {
+            if(type == Properties.HORSE)
+            {
+                return Instantiate(kittenModel, pos, Quaternion.identity);
+            }
+            else if(type == Properties.CAT)
+            {
+                return Instantiate(horseModel, pos, Quaternion.identity);
+            }
+            return null;
+        }
+
+        public void GrabAnimal(Player player, Animal animal)
+        {
+
+        }
+
+        public void DropAnimal(Player player)
+        {
+
+        }
+
+        public void KillAnimal(Player player, int keyModifier)
+        {
+
+        }
     }
 }
---
+++ Assets/Scripts/Properties.cs
@@ -0,0 +1,19 @@
+﻿using System.CodeDom;
+
+namespace Assets.Scripts.GameManager
+{
+    public static class Properties
+    {
+        public const int ANIMAL_TYPES = 2;
+        public const int CAT = 0;
+        public const int HORSE = 1;
+        public const int SHEEP = 2;
+        public const int CHICKEN = 3;
+        public const int GOAT = 4;
+        
+        public const int FIRST_PLAYER = 1;
+        public const int SECOND_PLAYER = 2;
+
+        public const float ANIMAL_SPAWN_RADIUS = 2f;
+    }
+}
