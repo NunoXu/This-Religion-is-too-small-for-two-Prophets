b9dc81d5891e337902e3cba5c9b71f73197cb264
Nuno Xu <nuno.xu@gmail.com>
2016-01-30 19:28:14 +0000

    Tweaked thunder and comet spells

--- Assets/Scripts/Spells/ThunderSpell.cs
+++ Assets/Scripts/Spells/ThunderSpell.cs
@@ -11,5 +11,31 @@ namespace Assets.Scripts.Spells
 
         public Collider targetCollider;
         public ParticleSystem particles;
+        public float startTime;
+        public float timeForThunder = 1.5f;
+
+        private bool done = false;
+
+        void Start()
+        {
+            startTime = Time.time;
+        }
+
+        void FixedUpdate()
+        {
+            if (done && particles.particleCount <=0)
+            {
+                GameObject.Destroy(this.gameObject);
+            }
+
+            if (!done && Time.time - startTime >= timeForThunder)
+            {
+                particles.Play();
+                done = true;
+            }
+
+            
+
+        }
     }
 }
