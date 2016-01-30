aebd3a7047744d97ba22dfe87901c77c702d8403
Luís Oliveira <luis_carlos_92@hotmail.com>
2016-01-30 00:40:29 +0000

    Initial skeleton of scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        //public fields to be set in Unity Editor
+        public GameManager.GameManager GameManager;
+        public GameObject GameCharacter;
+        
+        //private fields
+        private List<GameObject> PlayerSacrifices;
+        private GameObject CurrentSacrifice;
+
+        public void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                //stuff
+            }
+
+           
+        }
+
+      
+    }
+}
---
+++ Assets/Scripts/Sacrifices/Properties.cs
@@ -0,0 +1,14 @@
+﻿using System.CodeDom;
+
+namespace Assets.Scripts.GameManager
+{
+    public static class Properties
+    {
+        public const int CHICKEN = 0;
+        public const int COW = 1;
+        public const int DUCK = 2;
+        public const int SHEEP = 3;
+        public const int HORSE = 4;
+        public const int CAT = 5;
+    }
+}
