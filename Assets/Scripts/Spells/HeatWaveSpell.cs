07941dbc15b72f033d61b2fdcbe8b908572f5408
Nuno Xu <nuno.xu@gmail.com>
2016-01-30 23:20:10 +0000

    Added Invokers to cast the spells

---
+++ Assets/Scripts/Spells/HeatWaveSpell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Spells
+{
+    class HeatWaveSpell: Spell
+    {
+        public ParticleSystem ps;
+        public float startTime;
+        public float fireTime = 5f;
+
+        private bool done = true;
+
+        public void Invoke()
+        {
+            startTime = Time.time;
+            ps.Play();
+            done = false;
+        }
+
+        void Update()
+        {
+            if (!done && Time.time - startTime >= fireTime) {
+                ps.Stop();
+                done = true;
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Spells/Invokers/HeatWaveInvoker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Spells.Invokers
+{
+    class HeatWaveInvoker : Invoker
+    {
+        public GameObject[] Trees;
+        
+        public override void Invoke()
+        {
+            foreach (GameObject tree in Trees)
+            {
+                HeatWaveSpell spell = tree.GetComponent<HeatWaveSpell>();
+                spell.Invoke();
+            }    
+        }
+    }
+}
